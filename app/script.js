aebd0720b382903a8c6c4b807f39eb2c0739e8d4
ChristianScarabelli <scachmabol@hotmail.it>
2024-11-06 16:48:21 +0100

    ADD: js call and functions to create photo card

--- app/script.js
+++ app/script.js
@@ -1,1 +1,50 @@
 console.log('Photo Blog')
+
+// DOM 
+const photosContainer = document.getElementById('photo_container')
+
+
+const BASE_URL = 'https://jsonplaceholder.typicode.com/'
+let url_body = 'photos'
+
+const endpoint = BASE_URL + url_body
+
+axios
+    .get(endpoint, {
+        params: {
+            _limit: 6,
+        },
+    })
+    .then((res) => {
+        const photos = res.data  // variabile con i dati della chiamata
+
+        appendPhotos(photos, photosContainer)
+    })
+    .catch((err) => {
+        console.log(err)
+    })
+
+const appendPhotos = (photos, root) => {
+
+    photos.forEach(photo => {
+        const { url, title } = photo
+
+        const photoCardHtml = `
+        <div class="col col-4">
+            <div class="photo_card_main">
+                <figure class="photo_card_img">
+                    <img id="photo_img" src="${url}" alt="Immagine non disponibile">
+                    <img class="photo_pin" src="./img/pin.svg" alt="30">
+                </figure>
+                <div class="photo_card_text">
+                    <p>
+                        ${title}
+                    </p>
+                </div>
+            </div>
+        </div>
+        `
+        root.innerHTML += photoCardHtml
+    })
+}
+
