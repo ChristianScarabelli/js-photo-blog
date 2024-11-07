9f10d108a34f473304cac8298b90593582f66679
ChristianScarabelli <scachmabol@hotmail.it>
2024-11-07 12:12:20 +0100

    ADD: overlay button/card logic

--- app/script.js
+++ app/script.js
@@ -3,7 +3,6 @@ console.log('Photo Blog')
 // DOM 
 const photosContainer = document.getElementById('photo_container')
 
-
 const BASE_URL = 'https://jsonplaceholder.typicode.com/'
 let url_body = 'photos'
 
@@ -18,22 +17,22 @@ axios
     .then((res) => {
         const photos = res.data  // variabile con i dati della chiamata
         console.log(photos)
-        appendPhotos(photos, photosContainer)
+        appendPhotos(photos, photosContainer) // Chiamo la funzione per appendere in HTML le card
+        addCardClickEventListeners() // Chiamo la funzione per gli eventi dei click alle card e al bottone
     })
     .catch((err) => {
         console.log(err)
     })
 
+// Funzione per generare le cards in HTML
 const appendPhotos = (photos, root) => {
-
     photos.forEach(photo => {
         const { url, title } = photo
-
         const photoCardHtml = `
         <div class="col col-4">
             <div class="photo_card_main">
                 <figure class="photo_card_img">
-                    <img id="photo_img" src="${url}" alt="Immagine non disponibile">
+                    <img class="photo_img" src="${url}" alt="Immagine non disponibile">
                     <img class="photo_pin" src="./img/pin.svg" alt="30">
                 </figure>
                 <div class="photo_card_text">
@@ -48,3 +47,29 @@ const appendPhotos = (photos, root) => {
     })
 }
 
+// LOGICA DEL CLICK AL BOTTONE E DEL CLICK ALLE CARD 
+// Dom
+const closePhotoButtonEl = document.getElementById('close_photo_btn')
+const overlayEl = document.getElementById('overlay')
+
+// Evento al click bottone per chiudere l'overlay
+closePhotoButtonEl.addEventListener('click', () => {
+    overlayEl.classList.add('display_none')
+})
+
+// Funzione per aggiungere evento click a tutte le card
+const addCardClickEventListeners = () => {
+    const mainPhotoCardEls = document.querySelectorAll('.photo_card_main')
+
+    mainPhotoCardEls.forEach(card => {
+        card.addEventListener('click', () => {
+            overlayEl.classList.remove('display_none')
+            document.body.style.overflow = 'hidden'
+        })
+    })
+}
+
+// Evento al click dell'overlay per chiuderlo anche non cliccando sul tasto
+overlayEl.addEventListener('click', (event) => {
+    overlayEl.classList.add('display_none')
+})
